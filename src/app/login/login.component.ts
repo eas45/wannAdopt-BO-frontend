dff5f2c9cf7e44023cd08ee0a86adf367411a726
Esteban Antón Sellés <eas45@alu.ua.es>
2022-12-04 17:47:11 +0100

    Reactive form in login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  loginForm = this.fb.group({
+    nickname: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  public get nickname() { return this.loginForm.value.nickname; }
+  public get password() { return this.loginForm.value.password; }
+
+  constructor(private fb: FormBuilder) { }
+
+  onSubmit() {
+    console.log('Submited');
+  }
 
 }
