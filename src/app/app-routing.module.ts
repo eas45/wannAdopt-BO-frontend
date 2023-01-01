8a4dcfbcf8a88ea7f60675bc46e32f0dac454e0e
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-01 17:33:34 +0100

    Cambia las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
   { path: 'home', component: HomepageComponent }
 ]
 
