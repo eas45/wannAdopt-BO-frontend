1ac464e4fb8652a19dd3c0b2519c8b12107a7ae7
Esteban Antón Sellés <eas45@alu.ua.es>
2023-01-01 18:33:57 +0100

    Implementa control de sesión y cierre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StorageService } from './services/storage/storage.service';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wannAdopt-BO-frontend';
+  isLoggedIn: boolean = false;
+  email: string = '';
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      this.email = user.email;
+    }
+  }
+  
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+
+        window.location.reload();
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 }
